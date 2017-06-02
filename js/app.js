1e5408ce3631bcd15ff66b90db4ec3e2e7dacab6
nancyzj <ancizj@hotmail.com>
2017-06-02 12:06:47 -0500

    cambio de titulo

--- js/app.js
+++ js/app.js
@@ -1,23 +1,46 @@
+
+var contador = 0;
+var contadorLetras = 140;
 var cargarPagina = function () {
 	//obtener elementos
-	var btn = $('#agregar');
-	//funcionalidad 
-	btn.click(imprimir);
+	$('#mensaje').keyup(desabilitarBtn);
+	$('#agregar').click(imprimir);
 }
-	function imprimir(e){
+	var imprimir = function(e){
 		e.preventDefault();
-	// obtener elementos
-	var lienzo = $('#lienzo');
-	var txtMensaje = $('#mensaje');
-	//crear elementos
-	var parrafo = $('<p />');
-	//estilos a los elementos 
-	parrafo.addClass('jumbotron')
-	//agregar elementos 
-	parrafo.text(txtMensaje.val());
-	//agregar 
-	lienzo.append(parrafo);
-	txtMensaje.val("");
+		//quiero que texto reciba a textarea
+		//eq(1) te da el elemento que tiene ese indice
+		var texto = $(this).prev().children().eq(1); 
+		console.log(texto);
+		//crear variables
+		var identificador = 'parrafo' + contador; 
+		// obtener elementos
+		var lienzo = $('#lienzo');
+		//crear elementos
+		var parrafo = $("<p />", { "class": "jumbotron" });
+		console.log(parrafo);
+		//estilos a los elementos 
+		// parrafo.addClass('jumbotron')
+		parrafo.attr("id",identificador);
+		//agregar elementos 
+		parrafo.text(texto.val());
+		//agregar 
+		lienzo.append(parrafo);
+		texto.val("");
+		contador++;
+		console.log('de boton' + contador);
+	}
 
-}
+	var desabilitarBtn = function(){
+		contadorLetras --;
+		if(contadorLetras >= 1){
+			btn.removeClass('disabled');
+		}else{
+			btn.addClass('disabled');
+		}
+		console.log('este de letras' + contadorLetras);
+
+	}
+
+
 $(document).ready(cargarPagina);
